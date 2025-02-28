e1b3925739253e58b2cde3311fd1cf1dcafef916
Tingjun Su <37220232203813@stu.xmu.edu.cn>
2025-02-28 14:42:34 +0800

    。。

--- 01 - 常用在线模板汇总/02 - 图、树、网络流/04 - LCA（无权图树上倍增解）.cpp
+++ 01 - 常用在线模板汇总/02 - 图、树、网络流/04 - LCA（无权图树上倍增解）.cpp
@@ -1,49 +1,66 @@
-struct Tree {
+struct Tree
+{
     int n;
-    vector<vector<int>> ver, val;
-    vector<int> lg, dep;
-    Tree(int n) {
-        this->n = n;
-        ver.resize(n + 1);
-        val.resize(n + 1, vector<int>(30));
+    vector<vector<int>> adj;       // adjacency list
+    vector<vector<int>> ancestors; // ancestors at different levels
+    vector<int> lg;                // precomputed log values
+    vector<int> depth;             // u depths
+    Tree(int n) : n(n)
+    {
+        adj.resize(n + 1);
+        ancestors.resize(n + 1, vector<int>(30));
         lg.resize(n + 1);
-        dep.resize(n + 1);
-        for (int i = 1; i <= n; i++) { //预处理 log
+        depth.resize(n + 1);
+        for (int i = 1; i <= n; i++)
+        { // 预处理 log
             lg[i] = lg[i - 1] + (1 << lg[i - 1] == i);
         }
     }
-    void add(int x, int y) { // 建立双向边
-        ver[x].push_back(y);
-        ver[y].push_back(x);
+    void add(int u, int v)
+    { // 建立双向边
+        adj[u].push_back(v);
+        adj[v].push_back(u);
     }
-    void dfs(int x, int fa) {
-        val[x][0] = fa; // 储存 x 的父节点
-        dep[x] = dep[fa] + 1;
-        for (int i = 1; i <= lg[dep[x]]; i++) {
-            val[x][i] = val[val[x][i - 1]][i - 1];
+    void buildAncestors(int u, int parent)
+    {
+        ancestors[u][0] = parent; // 储存 x 的父节点
+        depth[u] = depth[parent] + 1;
+        for (int i = 1; i <= lg[depth[u]]; i++)
+        {
+            ancestors[u][i] = ancestors[ancestors[u][i - 1]][i - 1];
         }
-        for (auto y : ver[x]) {
-            if (y == fa) continue;
-            dfs(y, x);
+        for (auto v : adj[u])
+        {
+            if (v == parent)
+                continue;
+            buildAncestor(v, u);
         }
     }
-    int lca(int x, int y) {
-        if (dep[x] < dep[y]) swap(x, y);
-        while (dep[x] > dep[y]) {
-            x = val[x][lg[dep[x] - dep[y]] - 1];
+    int findLCA(int u, int v)
+    {
+        if (depth[u] < depth[v])
+            swap(u, v);
+        while (depth[u] > depth[v])
+        {
+            u = ancestors[u][lg[depth[u] - depth[v]] - 1];
         }
-        if (x == y) return x;
-        for (int k = lg[dep[x]] - 1; k >= 0; k--) {
-            if (val[x][k] == val[y][k]) continue;
-            x = val[x][k];
-            y = val[y][k];
+        if (u == v)
+            return u;
+        for (int level = lg[depth[u]] - 1; level >= 0; level--)
+        {
+            if (ancestors[u][level] == ancestors[v][level])
+                continue;
+            u = ancestors[u][level];
+            v = ancestors[v][level];
         }
-        return val[x][0];
+        return ancestors[u][0];
     }
-    int clac(int x, int y) { // 倍增查询两点间距离
-        return dep[x] + dep[y] - 2 * dep[lca(x, y)];
+    int getDistance(int u, int v)
+    { // 倍增查询两点间距离
+        return depth[u] + depth[v] - 2 * depth[findLCA(u, v)];
     }
-    void work(int root = 1) { // 在此初始化
-        dfs(root, 0);
+    void initialize(int root = 1)
+    { // 在此初始化
+        buildAncestor(root, 0);
     }
 };
--- 03 - jiangly模板收集/04 - 数据结构/07C - Splay.cpp
+++ 03 - jiangly模板收集/04 - 数据结构/07C - Splay.cpp
@@ -1,20 +1,24 @@
 /**   Splay
  *    2024-06-24: https://cf.dianhsu.com/gym/105229/submission/267199687?version=1.5
-**/
+ **/
 constexpr int D = 27;
-struct Info {
-    int up[D][2] {};
-    int down[D][2] {};
+struct Info
+{
+    int up[D][2]{};
+    int down[D][2]{};
     int t = 0;
     i64 ans = 0;
 };
 
-Info operator+(const Info &a, const Info &b) {
+Info operator+(const Info &a, const Info &b)
+{
     Info c;
     c.t = a.t ^ b.t;
     c.ans = a.ans + b.ans;
-    for (int i = 0; i < D; i++) {
-        for (int j = 0; j < 2; j++) {
+    for (int i = 0; i < D; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
             c.ans += (1LL << i) * a.down[i][j] * b.up[i][j ^ 1];
             c.up[i][j] += a.up[i][j] + b.up[i][j ^ (a.t >> i & 1)];
             c.down[i][j] += b.down[i][j] + a.down[i][j ^ (b.t >> i & 1)];
@@ -22,44 +26,54 @@ Info operator+(const Info &a, const Info &b) {
     }
     return c;
 }
-struct Node {
+struct Node
+{
     Node *ch[2], *p;
     Info val;
     Info tot;
     int cnt[D][2];
     i64 pair[D][2];
     i64 sum;
-    Node() : ch{nullptr, nullptr}, p(nullptr), cnt {}, pair {}, sum {} {}
+    Node() : ch{nullptr, nullptr}, p(nullptr), cnt{}, pair{}, sum{} {}
 };
-void pull(Node *t) {
-    t->tot = (t->ch[0] ? t->ch[0]->tot : Info {}) + t->val + (t->ch[1] ? t->ch[1]->tot : Info {});
+void pull(Node *t)
+{
+    t->tot = (t->ch[0] ? t->ch[0]->tot : Info{}) + t->val + (t->ch[1] ? t->ch[1]->tot : Info{});
 }
-bool isroot(Node *t) {
+bool isroot(Node *t)
+{
     return t->p == nullptr || (t->p->ch[0] != t && t->p->ch[1] != t);
 }
-int pos(Node *t) {
+int pos(Node *t)
+{
     return t->p->ch[1] == t;
 }
-void rotate(Node *t) {
+void rotate(Node *t)
+{
     Node *q = t->p;
     int x = !pos(t);
     q->ch[!x] = t->ch[x];
-    if (t->ch[x]) {
+    if (t->ch[x])
+    {
         t->ch[x]->p = q;
     }
     t->p = q->p;
-    if (!isroot(q)) {
+    if (!isroot(q))
+    {
         q->p->ch[pos(q)] = t;
     }
     t->ch[x] = q;
     q->p = t;
     pull(q);
 }
-void update(Node *t) {
+void update(Node *t)
+{
     t->val.ans = t->val.t + t->sum;
-    for (int i = 0; i < D; i++) {
+    for (int i = 0; i < D; i++)
+    {
         t->val.ans += (1LL << i) * t->pair[i][t->val.t >> i & 1];
-        for (int j = 0; j < 2; j++) {
+        for (int j = 0; j < 2; j++)
+        {
             t->val.up[i][j] = t->cnt[i][j ^ (t->val.t >> i & 1)];
             t->val.down[i][j] = t->cnt[i][j ^ (t->val.t >> i & 1)];
         }
@@ -68,12 +82,18 @@ void update(Node *t) {
     }
     pull(t);
 }
-void splay(Node *t) {
-    while (!isroot(t)) {
-        if (!isroot(t->p)) {
-            if (pos(t) == pos(t->p)) {
+void splay(Node *t)
+{
+    while (!isroot(t))
+    {
+        if (!isroot(t->p))
+        {
+            if (pos(t) == pos(t->p))
+            {
                 rotate(t->p);
-            } else {
+            }
+            else
+            {
                 rotate(t);
             }
         }
@@ -81,45 +101,61 @@ void splay(Node *t) {
     }
     pull(t);
 }
-void add(Node *t, Info s) {
-    for (int i = 0; i < D; i++) {
-        for (int x = 0; x < 2; x++) {
+void add(Node *t, Info s)
+{
+    for (int i = 0; i < D; i++)
+    {
+        for (int x = 0; x < 2; x++)
+        {
             t->pair[i][x] += s.up[i][1 ^ x];
-            for (int j = 0; j < 2; j++) {
+            for (int j = 0; j < 2; j++)
+            {
                 t->pair[i][x] += t->cnt[i][j] * s.up[i][j ^ 1 ^ x];
             }
         }
-        for (int j = 0; j < 2; j++) {
+        for (int j = 0; j < 2; j++)
+        {
             t->cnt[i][j] += s.up[i][j];
         }
     }
     t->sum += s.ans;
 }
-void del(Node *t, Info s) {
+void del(Node *t, Info s)
+{
     t->sum -= s.ans;
-    for (int i = 0; i < D; i++) {
-        for (int j = 0; j < 2; j++) {
+    for (int i = 0; i < D; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
             t->cnt[i][j] -= s.up[i][j];
         }
-        for (int x = 0; x < 2; x++) {
-            for (int j = 0; j < 2; j++) {
+        for (int x = 0; x < 2; x++)
+        {
+            for (int j = 0; j < 2; j++)
+            {
                 t->pair[i][x] -= t->cnt[i][j] * s.up[i][j ^ 1 ^ x];
             }
             t->pair[i][x] -= s.up[i][1 ^ x];
         }
     }
 }
-void access(Node *t, int v) {
+void access(Node *t, int v)
+{
     Info lst;
-    for (Node *i = t, *q = nullptr; i; q = i, i = i->p) {
+    for (Node *i = t, *q = nullptr; i; q = i, i = i->p)
+    {
         splay(i);
-        if (i->ch[1]) {
+        if (i->ch[1])
+        {
             add(i, i->ch[1]->tot);
         }
         i->ch[1] = q;
-        if (q) {
+        if (q)
+        {
             del(i, lst);
-        } else {
+        }
+        else
+        {
             i->val.t = v;
         }
         lst = i->tot;
